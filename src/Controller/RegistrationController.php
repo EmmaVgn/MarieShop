2aa421b24cf2d583244b288a82673607c2f8733c
EmmaVgn <emma.vigneron@outlook.fr>
2024-07-04 18:21:59 +0200

    update login page

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -11,12 +11,22 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mailer\Transport\Smtp\Auth\LoginAuthenticator;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 
 class RegistrationController extends AbstractController
 {
     #[Route('/register', name: 'register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, Security $security, EntityManagerInterface $entityManager): Response
+    public function register(
+        Request $request, 
+        UserPasswordHasherInterface $userPasswordHasher,
+        Security $security, 
+        UserAuthenticatorInterface $userAuthenticator, 
+        LoginAuthenticator $authenticator,
+        EntityManagerInterface $entityManager
+        ): Response
+
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -44,13 +44,13 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
-        if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
-            return new RedirectResponse($targetPath);
-        }
+      //  if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
+      //      return new RedirectResponse($targetPath);
+      //  }
 
         // For example:
         return new RedirectResponse($this->urlGenerator->generate('homepage'));
-       //  throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+         throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl(Request $request): string
