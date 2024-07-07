bb6ff55c81d360eab2546ebb9fd94902c1277506
EmmaVgn <emma.vigneron@outlook.fr>
2024-07-07 17:09:53 +0200

    Add cart

--- src/Form/SearchFormType.php
+++ src/Form/SearchFormType.php
@@ -2,23 +2,15 @@
 
 namespace App\Form;
 
-use App\Entity\Model;
+
 use App\Data\SearchData;
 use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Component\Form\Extension\Core\Type\DateIntervalType;
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
-use Symfony\Component\Validator\Constraints\Range;
-use Symfony\Component\Validator\Constraints\DateTime;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Component\Validator\Constraints\LessThanOrEqual;
-use Symfony\Component\Validator\Constraints\GreaterThanOrEqual;
 
 class SearchFormType extends AbstractType
 {
