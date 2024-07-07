936f3e450427262800739ab2c39d9b87be32b41a
EmmaVgn <emma.vigneron@outlook.fr>
2024-07-07 18:39:17 +0200

    Mailer

---
+++ src/Controller/MailerController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Models\Mail;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MailerController extends AbstractController
+{
+    #[Route('/mailer', name: 'mailer')]
+    public function index(): Response
+    {
+        $mail = new Mail();
+        $mail->send('marieShop@gmail.com', 'Marie Farjaud', 'Lien mot de passe', 'Voici votre lien');
+        return $this->render('mailer/index.html.twig', [
+            'controller_name' => 'MailerController',
+        ]);
+    }
+}
