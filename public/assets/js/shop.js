e096d34f16f99a7a5ad5acd7b57542b4d9afc888
EmmaVgn <emma.vigneron@outlook.fr>
2024-07-10 17:17:15 +0200

    fix bug

--- public/assets/js/shop.js
+++ public/assets/js/shop.js
@@ -1,22 +1,29 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+const quantityInput = document.getElementById('quantityInput');
 const incrementBtn = document.getElementById('incrementBtn');
 const decrementBtn = document.getElementById('decrementBtn');
-const quantityInput = document.getElementById('quantityInput');
-const addToCartBtn = document.getElementById('addToCartBtn');
 
 incrementBtn.addEventListener('click', () => {
-quantityInput.stepUp();
-});
-
-decrementBtn.addEventListener('click', () => {
-quantityInput.stepDown();
-});
-
-quantityInput.addEventListener('change', () => {
-if (quantityInput.value < 1) {
-quantityInput.value = 1;
-} else if (quantityInput.value > {{ product.stock }}) {
-quantityInput.value = {{ product.stock }};
+let currentQuantity = parseInt(quantityInput.value);
+if (currentQuantity < parseInt(quantityInput.max)) {
+quantityInput.value = currentQuantity + 1;
 }
 });
 
-addToCartBtn.href = addToCartBtn.href.replace('QUANTITY', quantityInput.value);
+decrementBtn.addEventListener('click', () => {
+let currentQuantity = parseInt(quantityInput.value);
+if (currentQuantity > 1) {
+quantityInput.value = currentQuantity - 1;
+}
+});
+
+quantityInput.addEventListener('change', () => {
+let currentQuantity = parseInt(quantityInput.value);
+if (currentQuantity < 1) {
+quantityInput.value = 1;
+} else if (currentQuantity > parseInt(quantityInput.max)) {
+quantityInput.value = parseInt(quantityInput.max);
+}
+});
+});
